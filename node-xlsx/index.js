bee62fe48d52369007156d73fe8fcf49c1265a44
leichangchun <springhubei@163.com>
2018-03-16 18:40:11 +0800

    还差交通方式分析

--- node-xlsx/index.js
+++ node-xlsx/index.js
@@ -6,9 +6,18 @@ const  fs = require('fs');
 //基础配置
 const baseUrl = 'https://lbs.gtimg.com/maplbs/qianxi/';
 let types = [0,1];  // 0/1  => 迁入/迁出
+let traffic = ['汽车','火车','飞机'];
 let tempSheetDate = [];
 let tempDate = [];
+let trafficSheetDate = [];
+let trafficDate = [];
 let y = 0; //全局索引
+
+// 指定时间范围  符合格式 YYYY-MM-DD,最新一天的为00000000
+let datelist = ['00000000', ...getTimeList('2018-03-09','2018-03-15','')];
+
+// 指定需要分析交通方式的城市code
+let trafficCode = 310000;
 
 //从city.xlsx中解析城市名和城市代码
 const citySheet = xlsx.parse(`${__dirname}/city.xlsx`);  
@@ -18,8 +27,7 @@ citySheet[0].data.slice(1).map( (value) => {
     cityNameList.push(value[0]);
     cityCodeList.push(value[1]);
 })
-// Todo : date数据后续需自动生成
-let datelist = ['00000000','20180314','20180313']
+
 //迁入、迁出分别遍历
 types.map( type => {
     //遍历请求数据
@@ -27,6 +35,13 @@ types.map( type => {
         let x = 0; //统计请求完成的数量
         tempSheetDate[code_index * 2 + type] = [];
         tempSheetDate[code_index * 2 + type].push([cityNameList[code_index]]) //每张表的表头,先不填日期，免得错位
+        //交通方式数据分析
+        if(code === trafficCode){
+            traffic.map( (tra,tra_index) => {
+                trafficSheetDate[traffic.length * type + tra_index] = [];
+                trafficSheetDate[traffic.length * type + tra_index].push([cityNameList[code_index]]);
+            })
+        }
         datelist.map( (date, col) => {
             request.get(`https://lbs.gtimg.com/maplbs/qianxi/${date}/${code}${type}6.js`).retry(3).buffer(true)
                 .end( (error,res) => {
@@ -42,7 +57,7 @@ types.map( type => {
                         })
                     }else{
                         real.map((value) => {
-
+                            //写入对应的城市
                             let isHas = tempSheetDate[code_index * 2 + type].find((city) => {
 
                                 if( city[0] === value[0]){
@@ -52,18 +67,51 @@ types.map( type => {
                                 }
                                 return false;
                             });
-
+                            //如果城市不存在，新建一行
                             if(!isHas){
                                 let tempRow = [value[0]];
-                                extendArray(tempRow,tempSheetDate[code_index * 2 + type][0].length -1, ' ');
+                                extendArray(tempRow,tempSheetDate[code_index * 2 + type][0].length - 1, ' ');
                                 tempRow.push(value[1])
                                 tempSheetDate[code_index * 2 + type].push(tempRow)
                             }
                         });
                     }
+
+                    //交通方式数据分析
+                    if(code === trafficCode){
+                        traffic.map( (tra,tra_index) => {
+                            trafficSheetDate[traffic.length * type + tra_index][0].push(date);
+                            if(trafficSheetDate[traffic.length * type + tra_index].lenght === 1){
+                                real.map((value) => {
+                                    trafficSheetDate[traffic.length * type + tra_index].push([value[0],value[tra_index]])
+                                })
+                            }else{
+                                real.map( (value) => {
+                                    //写入对应的城市
+                                    let isHas = trafficSheetDate[traffic.length * type + tra_index].find((city) => {
+
+                                        if( city[0] === value[0]){
+                                            extendArray(city,trafficSheetDate[traffic.length * type + tra_index][0].length -1, ' ');
+                                            city.push(value[tra_index])
+                                            return true;
+                                        }
+                                        return false;
+                                    });
+                                    //如果城市不存在，新建一行
+                                    if(!isHas){
+                                        let tempRow = [value[0]];
+                                        extendArray(tempRow,trafficSheetDate[traffic.length * type + tra_index][0].length - 1, ' ');
+                                        tempRow.push(value[tra_index])
+                                        trafficSheetDate[traffic.length * type + tra_index].push(tempRow)
+                                    }
+                                })
+                            }
+                        })
+                    }
                     x++;
                 })
         })
+        //一个单表的所有数据爬玩才能写入buffer中
         let singleSheet =  setInterval( () => {
             if(x === datelist.length){
                 console.log(`*** ${cityNameList[code_index]} 获取 ${type === 0 ?'迁入' : '迁出'} 数据完成***`);
@@ -71,6 +119,12 @@ types.map( type => {
                     name : `${cityNameList[code_index]}（${type === 0 ?'目的地' : '来源地'}）`,
                     data : tempSheetDate[code_index * 2 + type]
                 }
+                traffic.map( (tra,tra_index) => {
+                    trafficDate[traffic.length * type + tra_index] = {
+                        name: type === 0 ? `某地-${cityNameList[cityCodeList.indexOf(trafficCode)]}（来源地）（${traffic[tra_index]}）` : `${cityNameList[cityCodeList.indexOf(trafficCode)]} - 某地（来源地）（${traffic[tra_index]}）`,
+                        date: trafficSheetDate[traffic.length * type + tra_index]
+                    }
+                })
                 y++;
                 clearInterval(singleSheet);
             }
@@ -81,6 +135,7 @@ types.map( type => {
 let sheet = setInterval( () => {
     if( y === cityCodeList.length * 2){
         writeExcel(`腾讯迁徙爬取`,tempDate);
+        writeExcel(`腾讯迁徙爬取_交通方式`,trafficDate);
         clearInterval(sheet);
     }
 },1000);
@@ -105,4 +160,26 @@ function extendArray (array,length,value = " "){
     while(array.length < length) {
         array.push(value);
     }
+}
+//获取指定的时间范围
+function getTimeList(start = '2018-01-01',end = '2018-01-02',spliter = '-'){
+    let res = [];
+    let st = start.split('-');
+    let et = end.split('-');
+    let startTime = new Date(st[0],st[1] - 1, st[2]).getTime();
+    let endTime = new Date(et[0],et[1] -1, et[2]).getTime();
+    
+    for( let i = startTime ; i <= endTime ; ){
+        res.push(formatTime(i,''));
+        i += 24 * 60 * 60 * 1000;
+    }
+    return res.reverse();
+}
+
+function formatTime(time,spliter = '-'){
+    let date = new Date(time);
+    let year = date.getFullYear();
+    let month = (date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : '0' +  (date.getMonth() + 1);
+    let day = date.getDate() >= 10 ? date.getDate() : '0' + date.getDate();
+    return `${year}${spliter}${month}${spliter}${day}`
 }
